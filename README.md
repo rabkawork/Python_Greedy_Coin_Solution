7d05a2640d6597e5ed5ebd1116a5c1694ba6fa2d
Ahadian Akbar <rabkawork@gmail.com>
2020-10-10 23:35:09 +0700

    fixing solution

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Mencari solusi maksimal penukaran koin dengan algoritma Greedy
+# Mencari solusi minimum untuk penukaran koin dengan algoritma Greedy
 
 ## Language
 Python 3.6.9
@@ -6,6 +6,13 @@ Python 3.6.9
 ## Tools
 Visual Studio Code
 
-## Documentation
+## Skenario penukaran uang
 
-    
+* Koin yang ada => 100, 200, 500, 1000
+* Jumlah Uang -> 1500
+* Penukaran koin bisa dilakukan dengan beberapa cara 
+1. 100 + 100 + 100 ... + 100 (15 koin)
+2. 200 + 200 .... + 200 + 100 (8 Koin)
+3. 500 +  500 + 500 (3 Koin)
+4. 1000 + 500 (2 Koin) -> inilah solusi minimum penukaran koinnya
+
--- greedy_coin.py
+++ greedy_coin.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python3
-import sys
+import random
+print("Mencari solusi minimum penukaran koin dengan algoritma Greedy : ")
 
-print("Mencari solusi maksimal penukaran koin dengan algoritma Greedy : ")
-
-def fit(n, coin, needle):
-    return (n+coin) <= needle
-
-def greedy(coins, needle, MAXORMIN):
+def greedy(coins, needle):
     solution = []
-
-    if(MAXORMIN):
-        coins.sort(reverse=True)
     
-    #print(coins)
-    while (sum(solution) != needle):
-        for coin in coins:
-            total = sum(solution)
-            if fit(total,coin, needle):
-                solution.append(coin)
+    # sort urutan data dari terkecil ke terbesar
+    coins.sort(reverse=True)
+    
+    for coin in coins:
+        while (needle >= coin): 
+            needle -= coin
+            solution.append(coin)
+
+    if sum(solution) != needle:
+        solution = "Solusi tidak ditemukan"
+
     return solution
-    
-
 
 if __name__ == '__main__':
-    money = 2200
-    coins = [100,200,500,1000,2000]
+    money = 1050
+    coins = [100,200,500,1000]
 
     print("Koin : {0}".format(coins))
     print("Uang yang ditukar : {0}".format(money))
-    print("Solusi Minimal : {0}".format(greedy(coins, money, False)))
-    print("Solusi Maksimal : {0}".format(greedy(coins, money, True)))
+    print("Solusi Greedy : {0}".format(greedy(coins, money)))
