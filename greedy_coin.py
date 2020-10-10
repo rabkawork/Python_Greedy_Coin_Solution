cc601244580c4f8e8540a46d16dbba5b69bee8c4
Ahadian Akbar <rabkawork@gmail.com>
2020-10-10 13:53:40 +0700

    define function -> greedy to find the solutions

---
+++ greedy_coin.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#print("Mencari solusi maksimal penukaran koin dengan algoritma Greedy")
+
+def greedy(coins, max):
+    solution = []
+    return solution
+
+
+if __name__ == '__main__':
+    n = 1700
+    coins = [100,200,500,1000,2000]
+    
+    #print(coins)
