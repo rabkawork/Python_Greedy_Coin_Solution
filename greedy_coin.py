a9f699510fe98c24c31abf6eba1c81416cc415ff
Ahadian Akbar <rabkawork@gmail.com>
2020-10-10 14:43:19 +0700

    done and tested

--- greedy_coin.py
+++ greedy_coin.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
+import sys
 
-#print("Mencari solusi maksimal penukaran koin dengan algoritma Greedy")
+print("Mencari solusi maksimal penukaran koin dengan algoritma Greedy : ")
 
-def greedy(coins, max):
+def fit(n, coin, needle):
+    return (n+coin) <= needle
+
+def greedy(coins, needle, MAXORMIN):
     solution = []
+
+    if(MAXORMIN):
+        coins.sort(reverse=True)
+    
+    #print(coins)
+    while (sum(solution) != needle):
+        for coin in coins:
+            total = sum(solution)
+            if fit(total,coin, needle):
+                solution.append(coin)
     return solution
+    
 
 
 if __name__ == '__main__':
-    n = 1700
+    money = 2200
     coins = [100,200,500,1000,2000]
-    
-    #print(coins)
+
+    print("Koin : {0}".format(coins))
+    print("Uang yang ditukar : {0}".format(money))
+    print("Solusi Minimal : {0}".format(greedy(coins, money, False)))
+    print("Solusi Maksimal : {0}".format(greedy(coins, money, True)))
